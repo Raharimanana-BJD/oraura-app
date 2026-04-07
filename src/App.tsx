7828e709b9cd62e949769ad27457b75e76c0e9d0
RAHARIMANANA-BJD <brayjuanico@gmail.com>
2026-04-07 13:33:33 +0300

    init projet

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 export default function App() {
   return (
-    <div className="flex items-center justify-center min-h-svh w-full">App</div>
+    <div className="flex items-center justify-center min-h-svh w-full">
+      <h1 className="text-4xl text-primary">App</h1>
+    </div>
   );
 }
